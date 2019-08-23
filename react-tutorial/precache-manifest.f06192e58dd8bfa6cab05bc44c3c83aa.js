32a17e82928d61bc76b239583ee40baaa25b3043
Anton Redko <antonredko11116@gmail.com>
2019-08-23 13:09:05 +0300

    some

---
+++ react-tutorial/precache-manifest.f06192e58dd8bfa6cab05bc44c3c83aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9695aaddbfd8001e18591f96b00d584f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f82248c5cc9a0089034d",
+    "url": "/react-tutorial/static/css/main.ad8343fb.chunk.css"
+  },
+  {
+    "revision": "8a60d3552d6c0730d03e",
+    "url": "/react-tutorial/static/js/2.44d82ea3.chunk.js"
+  },
+  {
+    "revision": "f82248c5cc9a0089034d",
+    "url": "/react-tutorial/static/js/main.4575f4ad.chunk.js"
+  },
+  {
+    "revision": "1c9d7521ad772de5b9ff",
+    "url": "/react-tutorial/static/js/runtime~main.d5e8fc2e.js"
+  }
+]);
